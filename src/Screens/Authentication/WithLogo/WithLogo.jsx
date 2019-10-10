fc05b1c88f87f18a1be49780fe581c2b65b0b778
Akshay <akshay.patil@parambs.com>
2019-10-10 23:08:17 +0530

    withlogo folder structure created

---
+++ src/Screens/Authentication/WithLogo/WithLogo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class WithLogo extends Component {
+  render() {
+    return (
+      <div>
+        <div>Akshay</div>
+      </div>
+    );
+  }
+}
+
+export default WithLogo;
